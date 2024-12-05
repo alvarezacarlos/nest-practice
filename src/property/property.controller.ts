da12ee20241c586b83550b44d473454f10b8ab29
alvarezacarlos <alvaalbertocarlos@gmail.com>
2024-12-04 20:14:14 -0500

    add params bodyand http code

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, Body, HttpCode } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
@@ -6,11 +6,44 @@ export class PropertyController {
   findAll() {
     return { message: 'All Properties' };
   }
-
-  @Post()
-  create() {
-    return { message: 'This will create a property' };
+  
+  @Get(':id')
+  findOne(@Param('id') id: string) { // get the id param
+    return { message: `Find one by id: ${id}` };
   }
+  
+  @Get('/fetch/:id')
+  findOneById(@Param() id) { // get all the params. id is now an object which has all the parameters of the route
+    return id
+  }
+
+  //mutilple parameters in a route
+  @Get('/pull/:id/:slug')
+  // findOneItem(@Param('id') id:string, @Param('slug') slug:string) {
+  findOneItem(@Param() id) { // get all the params. id is now an object which has all the parameters of the route
+    return id
+  }
+
+  //post and body
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  //post and getting the param from the body directly
+  @Post('item')
+  createItem(@Body("name") name) {
+    return name;
+  }
+
+  
+  //post and returning the HTTP Code
+  @Post('record')
+  @HttpCode(202)
+  createRecord(@Body("name") name) {
+    return name;
+  }
+
 
   @Put()
   update() {
